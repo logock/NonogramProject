bc9f2c589181408adb72d8d2c145f57a7a65dcd5
Geckabor <alexkevesdi@yahoo.com>
2021-07-26 17:41:14 +0200

    Reworked code and added comments

--- Assets/Scripts/GridHandler.cs
+++ Assets/Scripts/GridHandler.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GridHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    public GameObject square;
+    public GameObject square;   // Square prefab... how all squares should look and act like
+    SquareHandler squareHandler;
     public int rows = 5;
     public int cols = 5;
 
-    public Vector2 startPosition = new Vector2(-6, 4);
+    public Vector2 startPosition = new Vector2(-6, 4);  // From where the squares start being generated.. hopefully can change this later
+
+    public bool leftClicked = false;    // Check if left mouse button is being held/clicked
+
+    public bool rightClicked = false;   // Check if right mouse button is being held/clicked
+
+    public IDictionary<int, Color> states = new Dictionary<int, Color>();   // Dictionary of all possible "states" aka colours the squares can be in the current game
+    public int selectedState = 1; // 0... white, 1... black  .... Will start with black colour selected as default
+    public bool colourBeingChanged = false;     // Check if colour is currently being changed in the grid, to avoid inversing colours (e.g: black to white AND white to black)
+    public int colourChangedTo = 0;     // Which colour the colours are being changed to.. necessary for some checks (should i explain more thoroughly?)
+
+
+
+    // Start is called before the first frame update
     void Start()
     {
+        // Add states/colours manually atm, will be generated per level in the future
+        states.Add(0, Color.white);
+        states.Add(1, Color.black);
+
+        // Get the squarehandler script from our square prefab and give it our grid
+        squareHandler = square.GetComponent<SquareHandler>();
+        squareHandler.grid = this;
+
+        // Generate rows * columns of squares, will need to add them into an array later probably  
         Vector2 pos = startPosition;
         for (int row = 0; row < rows; row++)
         {
@@ -29,6 +50,21 @@ public class GridHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if left mouse button is being clicked/held
+        if (Input.GetMouseButton(0) == true)
+        {
+            leftClicked = true;
+        }
+        else
+        {
+            leftClicked = false;
+            colourBeingChanged = false;
+        }
 
+        // Don't want to deal with bugs when people hold both mouse buttons, so right click only works when you aren't left clicking, might rework later
+        if (Input.GetMouseButton(0) == false && Input.GetMouseButton(1) == true)
+        {
+            rightClicked = true;
+        }
     }
 }
--- Assets/Scripts/SquareHandler.cs
+++ Assets/Scripts/SquareHandler.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class SquareHandler : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
-    public GameObject cross;
-    bool ctrlHeld = false;
-    bool pressedLeft = false;
-    bool isCrossed = false;
+    SpriteRenderer spriteRenderer;  // Need this to change colours
+    public GameObject cross;    // The "X" that happens when you want to cross the square out.. will either be enabled or disabled
+    public GridHandler grid;    // Reference to our grid
 
-    bool colorChanged = false; // Check to avoid multiple colour changes in one "swoop"
+    int state = 0; // 0... white, 1... black  .... 0 is default for now with squares starting off white. this will be expanded with colours in the future
 
-    public int state; // STATES: 1: WHITE  2: BLACK    3: CROSSED  ***NOT IMPLEMENTED***
-    // Start is called before the first frame update
+    bool colourChanged = false;     // Check if the colour of the square was changed while mouse is being held..
+                                    // colourChanged == true means that the square was clicked on and the left mouse button is being held
+
     void Start()
     {
+        // Get the squares spriterender and the cross isn't enabled by default
         spriteRenderer = GetComponent<SpriteRenderer>();
         cross.SetActive(false);
     }
@@ -23,94 +23,52 @@ public class SquareHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl)))
+        // If left click isn't being held, no colour is being changed actively
+        if (grid.leftClicked == false)
         {
-            ctrlHeld = true;
-        }
-        else
-        {
-            ctrlHeld = false;
-        }
-        if (Input.GetMouseButton(0) == true)
-        {
-            pressedLeft = true;
-        }
-        else
-        {
-            pressedLeft = false;
-            colorChanged = false;
+            colourChanged = false;
         }
     }
 
-
-
-    void OnMouseEnter()
+    /* So we want to check if left mouse is being clicked/held and also if this square's colour has been changed
+    colourChanged == true means that the square was clicked on and the left mouse button is currently being held
+    
+    If we're clicking/holding left mouse and haven't changed this square's colour yet, we want its colour to change
+    */
+    void OnMouseOver()  // If our mouse is on the square this function gets called
     {
-        if (colorChanged == false)
+        if (grid.leftClicked == true && colourChanged == false)
         {
-            if (pressedLeft == true && ctrlHeld == false)
+            if (grid.colourBeingChanged == true && grid.colourChangedTo == state)
             {
-                ChangeColor();
-                colorChanged = true;
-            }
-            else if (pressedLeft == true && ctrlHeld == true)
-            {
-                if (isCrossed == false)
-                {
-                    spriteRenderer.color = Color.white;
-                    cross.SetActive(true);
-                    isCrossed = true;
-                    colorChanged = true;
-                }
-                else
-                {
-                    spriteRenderer.color = Color.white;
-                    cross.SetActive(false);
-                    isCrossed = false;
-                    colorChanged = true;
-                }
-            }
-        }
-    }
-
-    void OnMouseDown()
-    {
-        if (ctrlHeld == true)
-        {
-            if (isCrossed == false)
-            {
-                spriteRenderer.color = Color.white;
-                cross.SetActive(true);
-                isCrossed = true;
-                colorChanged = true;
+                // To avoid inverse colouring:
+                // Do nothing if squares in the grid are being changed to a certain colour and that colour corresponds with the same colour of this square
             }
             else
             {
-                spriteRenderer.color = Color.white;
-                cross.SetActive(false);
-                isCrossed = false;
-                colorChanged = true;
+                // If above isn't the case, we want to change our square's colours to the selected state and signal that colours are currently being changed in the grid
+                changeState(grid.selectedState);
+                colourChanged = true;
+                grid.colourBeingChanged = true;
             }
         }
-        else
-        {
-            ChangeColor();
-            colorChanged = true;
-        }
+        // insert right click stuff here
     }
-    void ChangeColor()
+
+    void changeState(int toState)   // Change colours of the square
     {
-        if (spriteRenderer.color == Color.white)
+        if (toState == state)   // If the colour is currently the same as the selected one, it will inverse itself into white again
         {
-            spriteRenderer.color = Color.black;
-            cross.SetActive(false);
-            isCrossed = false;
+            state = 0;
+            spriteRenderer.color = grid.states[0];
+            grid.colourChangedTo = 0;
         }
-        else if (spriteRenderer.color == Color.black)
+        else    // Change to another colour otherwise; take colour from grid's state dictionary and notify grid which colour we are changing to
         {
-            spriteRenderer.color = Color.white;
-            cross.SetActive(false);
-            isCrossed = false;
+            state = toState;
+            spriteRenderer.color = grid.states[toState];
+            grid.colourChangedTo = toState;
         }
     }
 }
+
